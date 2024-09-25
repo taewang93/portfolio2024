b4085bae4d8268bd414e740029bd425c7aebbfe1
unknown <xodhkd334@naver.com>
2024-09-25 19:34:03 +0900

    0925

--- js/skill.js
+++ js/skill.js
@@ -4,12 +4,8 @@ document.addEventListener("scroll", function () {
   const targetY = document.querySelector(".skill_section");
   const skillTitleWrap = document.querySelector(".section_title_wrap");
   const skillTitle = document.querySelector(".skills_title");
-  // let opacityValue = 1 + (targetY.offsetTop - window.scrollY) / 200;
   let opacityValue = 1 - (window.scrollY - targetY.offsetTop - 100) / 250;
   let scaleValue = 1 - (window.scrollY - targetY.offsetTop - 100) / 1000;
-  // let scaleValue = 1 + (targetY.offsetTop - window.scrollY) / 600;
-
-  // console.log(1 - (window.scrollY - targetY.offsetTop - 100) / 1000);
 
   if (window.scrollY > targetY.offsetTop) {
     document.querySelector(".skill_section").classList.add("fixed_title");
@@ -27,36 +23,36 @@ document.addEventListener("scroll", function () {
     skillTitle.style.transform = `scale(${scaleValue})`;
   }
 
+  let pubOpaVal = (window.scrollY - targetY.offsetTop - WH) / 250;
+  let pubTopVal = (1 - (window.scrollY - targetY.offsetTop - WH) / 250) * 100;
+
   if (
     window.scrollY >= targetY.offsetTop + WH &&
     window.scrollY < targetY.offsetTop + WH + WH
   ) {
-    console.log("11");
     document.querySelector(".skill_image_publishing").classList.add("on");
-
     document.querySelector(".skill_image_publishing").classList.remove("off");
     document.querySelector(".skill_image_develop").classList.remove("on");
+
+    document.querySelector(".skill_publishing").style.opacity = pubOpaVal;
+    if (pubTopVal < 0) {
+      document.querySelector(".skill_publishing").style.top = 0;
+    } else {
+      document.querySelector(".skill_publishing").style.top = pubTopVal + "%";
+    }
   } else if (
     window.scrollY >= targetY.offsetTop + WH + WH &&
     window.scrollY < targetY.offsetTop + WH + WH + WH
   ) {
-    console.log("22");
     document.querySelector(".skill_image_publishing").classList.add("off");
     document.querySelector(".skill_image_develop").classList.add("on");
-
     document.querySelector(".skill_image_develop").classList.remove("off");
     document.querySelector(".skill_image_design").classList.remove("on");
   } else if (window.scrollY >= targetY.offsetTop + WH + WH + WH) {
-    console.log("33");
     document.querySelector(".skill_image_develop").classList.add("off");
     document.querySelector(".skill_image_design").classList.add("on");
   } else {
-    console.log("00");
     document.querySelector(".skill_image_publishing").classList.remove("on");
   }
-  console.log(
-    window.scrollY,
-    targetY.offsetTop + WH,
-    targetY.offsetTop + WH + WH
-  );
+  // console.log(pubTopVal);
 });
