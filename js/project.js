ca717f21cf630870a1a9b1a772f6fb09e5f9f402
unknown <xodhkd334@naver.com>
2024-10-07 22:32:53 +0900

    fixed project

---
+++ js/project.js
@@ -0,0 +1,9 @@
+const projectY = document.querySelector(".project_section");
+
+document.addEventListener("scroll", function () {
+  if (window.scrollY > projectY.offsetTop - 200) {
+    document.querySelector(".project_text").style.opacity = 1;
+  } else {
+    document.querySelector(".project_text").style.opacity = 0;
+  }
+});
