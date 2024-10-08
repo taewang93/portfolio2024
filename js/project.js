771a7cf0d7b455e590ff6651e0afa74d4b4f8665
unknown <xodhkd334@naver.com>
2024-10-08 09:30:55 +0900

    fixed visual

--- js/project.js
+++ js/project.js
@@ -1,7 +1,7 @@
 const projectY = document.querySelector(".project_section");
 
 document.addEventListener("scroll", function () {
-  if (window.scrollY > projectY.offsetTop - 200) {
+  if (window.scrollY > projectY.offsetTop - 400) {
     document.querySelector(".project_text").style.opacity = 1;
   } else {
     document.querySelector(".project_text").style.opacity = 0;
--- js/visual.js
+++ js/visual.js
@@ -7,10 +7,27 @@ const mouseOverVisual = (target) => {
     .querySelectorAll(".main_visual_button")
     [target].classList.remove("off");
   document.querySelectorAll(".main_visual_button")[target].classList.add("on");
+  document.querySelectorAll(".menu_link")[target].classList.add("hover");
 };
 const mouseOutVisual = (target) => {
   document
     .querySelectorAll(".main_visual_button")
     [target].classList.remove("on");
   document.querySelectorAll(".main_visual_button")[target].classList.add("off");
+  document.querySelectorAll(".menu_link")[target].classList.remove("hover");
+};
+
+const onClickVisual = (target) => {
+  if (target == "visual_section") {
+    console.log("aa");
+    window.scroll({
+      behavior: "smooth",
+      top: document.querySelector(`.${target}`).offsetTop - 80,
+    });
+  } else {
+    window.scroll({
+      behavior: "smooth",
+      top: document.querySelector(`.${target}`).offsetTop,
+    });
+  }
 };
